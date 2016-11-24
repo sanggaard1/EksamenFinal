91506ec074afb77831dfeb9d531cc6099f64ab92
sorenium <sorenium@gmail.com>
2016-11-24 14:04:43 +0100

    Config + config.json

---
+++ src/SDK/Config.java
@@ -0,0 +1,21 @@
+package SDK;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.FileReader;
+
+/**
+ * Created by Sanggaard on 24/11/2016.
+ */
+public class Config {
+
+    private static String serverUrl;
+
+    public static JsonObject initConfig(){
+
+
+
+    }
+
+}
