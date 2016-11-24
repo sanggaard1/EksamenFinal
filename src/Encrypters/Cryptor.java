ec4fbf392cb1d3fae1346dee1660252fb2dfe977
sorenium <sorenium@gmail.com>
2016-11-24 13:47:24 +0100

    Modelklasser udfyldes

---
+++ src/Encrypters/Cryptor.java
@@ -0,0 +1,7 @@
+package Encrypters;
+
+/**
+ * Created by Sanggaard on 24/11/2016.
+ */
+public class Cryptor {
+}
--- src/Model/User.java
+++ src/Model/User.java
@@ -4,4 +4,91 @@ package Model;
  * Created by Sanggaard on 24/11/2016.
  */
 public class User {
+
+    int userID;
+    String firstName;
+    String lastName;
+    String userName;
+    String email;
+    String password;
+    Boolean userType;
+
+    public User(){
+
+    }
+    // Construktur
+    public User(String firstName, String lastName, String userName, String email, String password, Boolean userType) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.userType = userType;
+
+    }
+    public User(int userID, String firstName, String lastName, String userName, String email, String password, Boolean userType) {
+        this.userID = userID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.userType = userType;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getUserType() {
+        return userType;
+    }
+
+    public void setUserType(Boolean userType) {
+        this.userType = userType;
+    }
 }
